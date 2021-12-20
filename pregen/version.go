ae634c6ff07d365bd62790bf7250c3effa1e44bd
Mark Delany <puz@bravo.emu.st>
2021-12-20 16:45:49 +1000

    Codecov needs pregen files

---
+++ pregen/version.go
@@ -0,0 +1,14 @@
+/*
+
+Package pregen contains files which are pre-generated for the build process and a small
+embed package to included the pregenerated files into the autoreverse executable.
+
+*/
+package pregen
+
+const (
+	// Version is auto-generated from ChangeLog.md
+	Version = "v1.0.1"
+	// ReleaseDate is also auto-generated from ChangeLog.md
+	ReleaseDate = "2021-12-14"
+)
